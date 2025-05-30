f024f2ef2c5019e5e9330080a8021e416e6e67a3
LatticeMage <LatticeMage@gmail.com>
2025-05-30 22:37:06 +0800

    good cli

--- serve.bat
+++ serve.bat
@@ -1,15 +1,1 @@
-setlocal
-
-:: Get the current folder name
-for %%a in (".") do set "foldername=%%~nxa"
-
-:: Construct the URL
-set "url=http://127.0.0.1:4001/%foldername%/"
-
-:: Start the browser with the constructed URL
-start %url%
-
-:: Start Jekyll server
-bundle exec jekyll serve --port 4001
-
-endlocal
+bundle exec jekyll serve --port 4001 --open-url
